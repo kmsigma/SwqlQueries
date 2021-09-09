de875aa8cff6badb4411ca81cc13f802d1166b3e
ksparenberg <kevin.sparenberg@solarwinds.com>
2021-09-09 15:59:06 -0500

    Added some more types - more TBD

--- Export-SwisCp.ps1
+++ Export-SwisCp.ps1
@@ -1,4 +1,22 @@
 ﻿# Export-SwisCp.ps1
+$ExecutionStartTime = ( Get-Date -Format 's' ).Replace(":", "-")
+
+<#
+
+Yet to be done:
+Entities of type:
+- Orion.SRM.ProviderCustomProperties
+- Orion.SRM.StorageArrayCustomProperties
+- Orion.SRM.StorageControllerCustomProperties
+- Orion.SRM.StorageControllerPortCustomProperties
+- Orion.SRM.VolumeCustomProperties
+- Orion.VIM.ClustersCustomProperties
+- Orion.VIM.DataCentersCustomProperties
+- Orion.VIM.DatastoresCustomProperties
+- Orion.VIM.HostsCustomProperties
+- Orion.VIM.VirtualMachinesCustomProperties
+
+#>
 
 # Select the location where we want to store the exported files
 $ExportPath = ".\CustomPropertyExports"
@@ -49,30 +67,39 @@ $TargetEntities = $ListOfCps | Select-Object -Property TargetEntity -Unique | Se
   This only needs to be done once before we start building the queries
 #>
 
+
+
 # To do this work we'll need some 'default' fields so we can identify what we're looking at
 $BaseCpFields = @{
-    "IPAM.GroupsCustomProperties"               = "$( $CpAlias ).GroupID, $( $CpAlias ).GroupNode.FriendlyName, $( $CpAlias ).GroupNode.Address, $( $CpAlias ).GroupNode.CIDR, $( $CpAlias ).GroupNode.GroupTypeText";
-    "Orion.AlertConfigurationsCustomProperties" = "$( $CpAlias ).AlertID, $( $CpAlias ).Alert.Name";
-    "Orion.GroupCustomProperties"               = "$( $CpAlias ).ContainerID, $( $CpAlias ).[Group].Name";
-    "Orion.NodesCustomProperties"               = "$( $CpAlias ).NodeID, $( $CpAlias ).Node.IPAddress, $( $CpAlias ).Node.Caption";
-    "Orion.NPM.InterfacesCustomProperties"      = "$( $CpAlias ).InterfaceID, $( $CpAlias ).Interface.FullName"
-    "Orion.ReportsCustomProperties"             = "$( $CpAlias ).ReportID, $( $CpAlias ).Report.Title"
+    "IPAM.GroupsCustomProperties"               = "$( $CpAlias ).Uri, $( $CpAlias ).GroupNode.FriendlyName, $( $CpAlias ).GroupNode.Address, $( $CpAlias ).GroupNode.CIDR, $( $CpAlias ).GroupNode.GroupTypeText"
+    "IPAM.NodesCustomProperties"                = "$( $CpAlias ).Uri, $( $CpAlias ).IPNode.SysName, $( $CpAlias ).IPNode.DnsBackward, $( $CpAlias ).IPNode.IPAddress, $( $CpAlias ).IPNode.MAC"
+    "Orion.AlertConfigurationsCustomProperties" = "$( $CpAlias ).Alert.Uri + '/CustomProperties' AS [Uri], $( $CpAlias ).Alert.Name"
+    "Orion.APM.ApplicationCustomProperties"     = "$( $CpAlias ).Application.Uri + '/CustomProperties' AS [Uri], $( $CpAlias ).Application.Node.Caption, $( $CpAlias ).Application.Name"
+    "Orion.GroupCustomProperties"               = "$( $CpAlias ).[Group].Uri + '/CustomProperties' AS [Uri], $( $CpAlias ).[Group].Name"
+    "Orion.NodesCustomProperties"               = "$( $CpAlias ).Node.Caption, $( $CpAlias ).Uri, $( $CpAlias ).Node.IPAddress"
+    "Orion.NPM.InterfacesCustomProperties"      = "$( $CpAlias ).Uri, $( $CpAlias ).Interface.FullName"
+    "Orion.ReportsCustomProperties"             = "$( $CpAlias ).Uri, $( $CpAlias ).Report.Title"
+    "Orion.SEUM.RecordingCustomProperties"      = "$( $CpAlias ).Recording.Uri + '/CustomProperties' AS [Uri], $( $CpAlias ).Recording.DisplayName, $( $CpAlias ).Recording.Description"
+    "Orion.VolumesCustomProperties"             = "$( $CpAlias ).Uri, $( $CpAlias ).Volume.Node.Caption AS [Node], $( $CpAlias ).Volume.Description, $( $CpAlias ).Volume.VolumeType"
 }
 
 # Some of the queries would benefit from filtering off some information
 $WhereClauses = @{
-    "IPAM.GroupsCustomProperties" = "WHERE $( $CpAlias ).GroupNode.CIDR <> 0";
-    "Orion.GroupCustomProperties" = "WHERE $( $CpAlias ).[Group].Owner <> 'Maps'";
+    "IPAM.GroupsCustomProperties" = "WHERE $( $CpAlias ).GroupNode.CIDR <> 0"
+    "Orion.GroupCustomProperties" = "WHERE $( $CpAlias ).[Group].Owner <> 'Maps'"
 }
 
 # This sorting is optional, but incredibly helpful
 $OrderByClauses = @{
-    "IPAM.GroupsCustomProperties"               = "ORDER BY $( $CpAlias ).GroupNode.ParentID, $( $CpAlias ).GroupNode.GroupType";
-    "Orion.AlertConfigurationsCustomProperties" = "ORDER BY $( $CpAlias ).Alert.Name";
-    "Orion.GroupCustomProperties"               = "ORDER BY $( $CpAlias ).[Group].Name";
-    "Orion.NodesCustomProperties"               = "ORDER BY $( $CpAlias ).Node.Caption, $( $CpAlias ).Node.IPAddress";
+    "IPAM.GroupsCustomProperties"               = "ORDER BY $( $CpAlias ).GroupNode.ParentID, $( $CpAlias ).GroupNode.GroupType"
+    "IPAM.NodesCustomProperties"                = "ORDER BY $( $CpAlias ).IpNode.IPAddressN"
+    "Orion.AlertConfigurationsCustomProperties" = "ORDER BY $( $CpAlias ).Alert.Name"
+    "Orion.APM.ApplicationCustomProperties"     = "ORDER BY $( $CpAlias ).Application.Name, $( $CpAlias ).Application.Node.Caption"
+    "Orion.GroupCustomProperties"               = "ORDER BY $( $CpAlias ).[Group].Name"
+    "Orion.NodesCustomProperties"               = "ORDER BY $( $CpAlias ).Node.Caption, $( $CpAlias ).Node.IPAddress"
     "Orion.NPM.InterfacesCustomProperties"      = "ORDER BY $( $CpAlias ).Interface.FullName"
     "Orion.ReportsCustomProperties"             = "ORDER BY $( $CpAlias ).Report.Title"
+    "Orion.SEUM.RecordingCustomProperties"      = "ORDER BY $( $CpAlias ).Recording.DisplayName"
 }
 #endregion Identifying Details/Filters/Sorting
 
@@ -82,12 +109,14 @@ ForEach ( $TargetEntity in $TargetEntities ) {
     $Fields = $ListOfCps | Where-Object { $_.TargetEntity -eq $TargetEntity }
     # Now the $Fields variable contains the names of the fields for the current Target Entity type
     # But, I want to use the Alias, so I'll need to do some clever convertion and then store them as strings (so I can use the -join operator)
-    $FieldsWithAlias = $Fields | Select-Object -Property @{ Name = 'Field'; Expression = { "$( $CpAlias ).$( $_.Field )" } } | Select-Object -ExpandProperty Field
+    # I also added an alias for CP_(FieldName) so in the exported CSV, it's obvious which fields are custom properties
+    $FieldsWithAlias = $Fields | Select-Object -Property @{ Name = 'Field'; Expression = { "$( $CpAlias ).$( $_.Field ) AS CP_$( $_.Field )" } } | Select-Object -ExpandProperty Field
     
     # The query takes the form:
-    # SELECT (Base Fields), (Fields From Custom Properties) FROM (Target Entity) (WHERE/filter clauses) (ORDER BY/sorting clauses)
+    # SELECT (Base Fields), (Fields From Custom Properties), (Custom Property's URI) FROM (Target Entity) (WHERE/filter clauses) (ORDER BY/sorting clauses)
     if ( $BaseCpFields[$TargetEntity] ) {
         $CpQuery = "SELECT $( $BaseCpFields[$TargetEntity] ), $( $FieldsWithAlias -join ", " ) FROM $TargetEntity AS $CpAlias $( $WhereClauses[$TargetEntity] ) $( $OrderByClauses[$TargetEntity] )"
+        Write-Verbose -Message "Execution: $CpQuery"
         $Results = Get-SwisData -SwisConnection $SwisConnection -Query $CpQuery
         if ( $Results ) {
             Write-Host "Exporting $( $Results.Count) record(s) from $TargetEntity to '$( $ExportPath )\$( $TargetEntity ).csv"
@@ -95,6 +124,7 @@ ForEach ( $TargetEntity in $TargetEntities ) {
         }
     }
     else {
+        "No 'Default' Custom Properties are defined for '$TargetEntity'" | Out-File -FilePath ( Join-Path -Path $ExportPath -ChildPath "Error_$( $ExecutionStartTime ).log" ) -Append
         Write-Error -Message "No 'Default' Custom Properties are defined for '$TargetEntity'" -RecommendedAction "Update the script to fix it"
     }
 }
